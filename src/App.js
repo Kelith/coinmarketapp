721a8d53ff0ee71229b8d5963e2ab591a43f92cb
Kelith <garofalodavide7@gmail.com>
2022-08-08 10:42:12 +0200

    Boilerplate components and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
-import {Navbar} from './components'
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components'
+import './App.css'
+import 'antd/dist/antd.css'
 
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
-
+        <Navbar />
       </div>
       <div className="main">
-        
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/exchanges" component={Exchanges} />
+              <Route exact path="/cryptocurrencies" component={Cryptocurrencies} />
+              <Route exact path="/crypto/:coinId" component={CryptoDetails} />
+              <Route exact path="/news" component={News} />
+            </Routes>
+          </div>
+        </Layout>  
       </div>
       <div className="footer">
         
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,20 @@ const Navbar = () => {
             </Typography.Title>
             {/* <Button className="menu-control-container"></Button> */}
         </div>
+        <Menu theme='dark'>
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="cryptocurrencies" icon={<FundOutlined />}>
+                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item key="exchanges" icon={<MoneyCollectOutlined />}>
+                <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item key="news" icon={<BulbOutlined />}>
+                <Link to="/news">News</Link>
+            </Menu.Item>
+        </Menu>
     </div>
   )
 }
