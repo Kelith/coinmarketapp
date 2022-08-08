6b06ce5b2d2428d15494624cda5ff2e4a95a24ab
Kelith <garofalodavide7@gmail.com>
2022-08-08 11:37:55 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -18,18 +18,27 @@ const App = () => {
             <Routes>
               <Route exact path="/" component={Homepage} />
               <Route exact path="/exchanges" component={Exchanges} />
-              <Route exact path="/cryptocurrencies" component={Cryptocurrencies} />
+              <Route exact path="/cryptocurrencies" component={Cryptocurrencies}/>
               <Route exact path="/crypto/:coinId" component={CryptoDetails} />
               <Route exact path="/news" component={News} />
             </Routes>
           </div>
-        </Layout>  
-      </div>
-      <div className="footer">
-        
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
+            CoinMarketAPP <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
