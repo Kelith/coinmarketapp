eaf036a13f2bd02827daa2a59b525ebfb395b8ee
Kelith <garofalodavide7@gmail.com>
2022-08-10 18:56:21 +0200

    Coin Details and charts

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,50 @@
+
+import React from 'react';
+import { Chart } from 'chart.js/auto'
+import { Line } from 'react-chartjs-2';
+import { Col, Row, Typography } from 'antd';
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+    coinHistory?.data?.history.forEach(({ timestamp, price }) => {
+        coinTimestamp.push(new Date(timestamp * 1000).toLocaleDateString());
+        coinPrice.push(price);
+    });
+
+    coinPrice.reverse();
+    coinTimestamp.reverse();
+  
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
+          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+        </Col>
+      </Row>
+      <Line data={data}  />
+    </>
+  );
+};
+
+export default LineChart;
