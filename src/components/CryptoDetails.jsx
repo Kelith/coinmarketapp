2bd4dff022a2cffb52407428ae18711adfab3c3b
Kelith <garofalodavide7@gmail.com>
2022-08-11 09:56:40 +0200

    Added Loader(Spinner)

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import Loader from './Loader'
 
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
 // import Loader from './Loader';
@@ -20,7 +21,7 @@ const CryptoDetails = () => {
   const cryptoDetails = data?.data?.coin;
   console.log({ coinHistory });
 
-  if (isFetching) return "<Loader />";
+  if (isFetching) return <Loader />;
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
